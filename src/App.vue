8011ee702ef808a0f34ef106a31f7ce6be61a8ef
konaOnTheHub <nagy.petike2@gmail.com>
2025-10-14 14:50:07 +0100

    added navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
-<script setup></script>
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <Navbar />
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+
+<template>
+    <nav class="w-full bg-">
+    </nav>
+</template>
