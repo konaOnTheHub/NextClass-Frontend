6007384fd5453018490fb1b9a077ecc5746c9d38
konaOnTheHub <nagy.petike2@gmail.com>
2025-10-15 16:34:36 +0100

    Now cards are dynamically placed in for every lesson available in lessons.json. Also added logic for availability  changing the spans colour based on how many free spaces there are.

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,23 +1,47 @@
 <script setup>
-    import { ref } from 'vue';
+    import {defineProps,ref,computed} from 'vue';
+    const props = defineProps ({
+        lesson: Object
+    });
 
-    const availabilityStatus = ref('Open');
-
+    const availability = computed(() => {
+        let numSpaces = props.lesson.spaces;
+        if (numSpaces <= 3 && numSpaces > 0) {
+            return {
+                text: "Limited",
+                colour: "bg-amber-600",
+                hoverColour: "hover:bg-amber-800",
+                numSpaces: numSpaces
+            };
+        } else if (numSpaces == 0) {
+            return {
+                text: "Closed",
+                colour: "bg-red-600",
+                hoverColour: "hover:bg-gray-800",
+                numSpaces: numSpaces
+            }
+        } else {
+            return {
+                text: "Open",
+                colour: "bg-green-600",
+                hoverColour: "hover:bg-green-800",
+                numSpaces: numSpaces
+            }
+        }
+    });
 </script>
 
 <template>
     <div class="h-48 w-full max-w-xs border-2 border-indigo-500 rounded-lg mx-auto relative bg-gray-800 shadow-lg">
         <div>
-            <h1 class="text-white pl-5 pt-5 font-bold text-2xl">Subject</h1>
-            <h3 class="text-gray-300 pl-5">Location</h3>
-            <h3 class="text-white absolute right-0 pr-5 font-bold text-lg">£20</h3>
+            <h1 class="text-white pl-5 pt-5 font-bold text-2xl">{{lesson.subject}}</h1>
+            <h3 class="text-gray-300 pl-5">{{lesson.location}}</h3>
+            <h3 class="text-white absolute right-0 pr-5 font-bold text-lg">£{{lesson.price}}</h3>
             <div class="h-16 bg-gray-950 absolute bottom-0 left-0 right-0 rounded-b-md flex items-center justify-between px-5">
-                <span v-if="availabilityStatus === 'Open'" class="bg-green-600 hover:bg-green-800 text-white text-sm font-semibold px-3 py-1 rounded-full">
-                    {{availabilityStatus}}
+                <span v-bind:class="`${ availability.colour } ${availability.hoverColour} text-white text-sm font-semibold px-3 py-1 rounded-full`">
+                    {{availability.text}}
                 </span>
-                <span v-if="availabilityStatus === 'Limited'" class=" bg-amber-600 hover:bg-amber-800 text-white text-sm font-semibold px-3 py-1 rounded-full">
-                    {{availabilityStatus}}
-                </span>
+                
                 <button class="text-white border-2 border-indigo-500 rounded-full px-4 py-1 hover:bg-indigo-500">Add to cart</button>
             </div>
         </div>
--- src/components/LessonCards.vue
+++ src/components/LessonCards.vue
@@ -1,16 +1,14 @@
 <script setup>
-    import Card from '@/components/Card.vue'
+    import Card from '@/components/Card.vue';
+    import { ref } from 'vue';
+    import lessonData from '@/lessons.json'
+
+    const lessons = ref(lessonData);
 </script>
 
 <template>
     <div class="grid gap-10 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card v-for="lesson in lessonData" :key="lesson.id" :lesson="lesson" />
+
     </div>
 </template>
