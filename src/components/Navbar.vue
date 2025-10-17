aad9d00519693b5b779669e45728cdec8875398b
konaOnTheHub <nagy.petike2@gmail.com>
2025-10-17 01:52:30 +0100

    Implemented dropdown menus for both large and small screens. Made small screen search button expand into a mini input field. Furthermore the dropdowns now dissapear on click outside.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,59 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, nextTick, onMounted } from 'vue';
+
+//States (Search)
 const showSearch = ref(false);
+const showSearchDropdown = ref(false);
+const showSmallSearchDropdown = ref(false);
+
+//Element ref (Search)
+const smallSearchResults = ref(null);
+const smallSearchInput = ref(null);
+const searchButton = ref(null);
+
+const largeSearchInput = ref(null);
+const largeSearchResults = ref(null);
+
+function handleShowSearch() {
+    showSearch.value = !showSearch.value;
+    if (showSearch.value) {
+        nextTick(() => {
+            smallSearchInput.value?.focus();
+        });
+    } else {
+        showSmallSearchDropdown.value = false;
+    }
+};
+function handleClickOutside(event) {
+    const input = smallSearchInput.value;
+    const button = searchButton.value;
+    const dropdown = smallSearchResults.value;
+
+    //Large dropdown
+    const lgInput = largeSearchInput.value;
+    const lgDropdown = largeSearchResults.value;
+
+    if (
+        input && input.contains(event.target) ||
+        button && button.contains(event.target) ||
+        dropdown && dropdown.contains(event.target) ||
+        lgInput && lgInput.contains(event.target) ||
+        lgDropdown && lgDropdown.contains(event.target)
+    ) {
+        // Click is inside, do nothing
+        return;
+    }
+
+    // Click is outside, close
+    showSearch.value = false;
+    showSmallSearchDropdown.value = false;
+    showSearchDropdown.value = false;
+};
+
+onMounted(() => {
+    document.addEventListener('click', handleClickOutside)
+});
+
 </script>
 
 <template>
@@ -12,30 +65,46 @@ const showSearch = ref(false);
 
         <!-- Middle Search Field -->
         <div class="absolute left-1/2 transform -translate-x-1/2">
-            <input type="text" placeholder="Search classes..."
+            <input type="text" @focusin="showSearchDropdown = true"
+                ref="largeSearchInput"
+                placeholder="Search classes..."
                 class="w-96 h-10 px-4 text-sm text-white rounded-lg ring-2 ring-indigo-500 focus:outline-none hidden lg:block" />
-            <button @click="showSearch = !showSearch" class=" w-8 h-8 m-3 block lg:hidden bg-indigo-500 rounded-full p-1.5">
-                <img src="/src/assets/search.svg" alt="Search">
-            </button>
+
         </div>
+        <!--Spacer that ensures right panel sticks to the right. Was using ml-auto before this and that caused problems.-->>
+        <div class="flex-1"></div>
 
         <!-- Right Panel -->
-        <div class="flex items-center h-full ml-auto">
-            
-            <a href="#" class="text-1xl text-white p-1 m-3">Home</a>
-            <a href="#" class="text-1xl text-white p-1 m-3">Contact</a>
-            <a href="#" class="text-1xl text-white p-1 m-3">About</a>
+        <div class="flex items-center h-full ">
+            <div class="relative w-full h-8 mr-8">
+
+                <input v-if="showSearch" ref="smallSearchInput" @focusin="showSmallSearchDropdown = true"
+                    placeholder="Search"
+                    class="w-60 h-8 pl-10 rounded-full border-2 border-indigo-500 text-white focus:outline-none block lg:hidden">
+                <button @click="handleShowSearch" ref="searchButton" tabindex="0"
+                    class="absolute left-0 top-0 w-8 h-8 block lg:hidden bg-indigo-500 rounded-full p-1.5">
+                    <img src="/src/assets/search.svg" alt="Search">
+                </button>
+                <!--Search-->
+                <div v-if="showSmallSearchDropdown"
+                    ref="smallSearchResults"
+                    class="w-full h-40 bg-gray-950 border-2 border-gray-600 mt-[11px] rounded-b-lg opacity-95">
+
+                </div>
+
+            </div>
+            <a v-if="!showSearch" href="#" class="text-1xl text-white p-1 m-3">Home</a>
+            <a v-if="!showSearch" href="#" class="text-1xl text-white p-1 m-3">Contact</a>
+            <a v-if="!showSearch" href="#" class="text-1xl text-white p-1 m-3">About</a>
+
         </div>
     </nav>
-     <!-- Small screen dropdown -->
-   <div 
-        v-show="showSearch" 
-        class="fixed top-14 left-0 w-full bg-gray-900 p-3 flex justify-center lg:hidden z-1"
-      >
-        <input 
-          type="text" 
-          placeholder="Search classes..." 
-          class="w-full h-10 px-4 text-sm text-white rounded-lg ring-2 ring-indigo-500 focus:outline-none bg-gray-800"
-        />
-      </div>
+
+    <!-- Search results big screen -->
+    <div v-if="showSearchDropdown"
+        ref="largeSearchResults"
+        class="fixed left-1/2 transform -translate-x-1/2 flex justify-center opacity-95 border-2 rounded-b-lg border-gray-600 z-1 top-13 h-32 w-96 bg-gray-950 text-gray-600">
+        <p>Search by name</p>
+    </div>
+   
 </template>
